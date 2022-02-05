03c82ea2fb138f3aba1042c41e32829272ff8a0a
Andrew Farmer <andrewfarmer13@gmail.com>
2022-02-05 16:35:53 -0600

    Update RSA.py

--- RSA.py
+++ RSA.py
@@ -1,1 +1,35 @@
 ##Contributers: Andrew Farmer, Clayton 
+
+
+##Generates both public and private keys
+def generate():
+    print("test")
+    
+##Generate digital signature
+def digital():
+    print("test")
+
+##Authenticates the digital signature
+def authenticate():
+    print("test")
+    
+##Encrypts with a public key
+def encrypt():
+    print("test")
+    
+##Decrypts with a private key
+def decrypt():
+    print("test")
+    
+
+
+##Driver Function
+def main():
+    generate()
+    digital()
+    authenticate()
+    encrypt()
+    decrypt()
+    
+main()
+    

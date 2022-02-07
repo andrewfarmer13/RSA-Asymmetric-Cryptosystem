22da00eab2eda5e16d1f8783f5ec8d83f3d99058
ClaytonMcEntire <83796599+ClaytonMcEntire@users.noreply.github.com>
2022-02-07 10:38:16 -0600

    Creation of menu functions

--- RSA.py
+++ RSA.py
@@ -21,7 +21,28 @@ def encrypt():
 def decrypt():
     print("test")
     
-
+## Main Selection Menu
+def menu():
+    print("Please select your user type:")
+    print("1. Public User \n2. The owner of the keys \n3. Exit Program")
+    choice = None
+    choice = int(input("\nPlease Enter your choice: "))  
+    if choice == 1:
+        publicusr()
+## Public User
+def publicusr():
+     print("\nAs a public user, what would you like to do?")
+     print("1. Send an encrypted message \n2. Authenticate a digital signature \n3. Exit")
+     choice = None
+     choice = int(input("Enter your choice: "))
+     if choice ==1:
+        string = None
+        string = str(input("Enter a message: "))
+                
+        ## CALL ENCRYPT AND send string
+        print("Message encrypted and sent.")
+     elif choice == 3:
+        menu()
 
 ##Driver Function
 def main():
@@ -30,25 +51,9 @@ def main():
     authenticate()
     encrypt()
     decrypt()
+   ## menu()
+   ## publicusr()
     
 main()
-
- 
-print("Please select your user type:")
-print("1. Public User \n2. The owner of the keys \n3. Exit Program")
-choice = None
-choice = int(input("\nPlease Enter your choice: "))
-    
-if choice == 1:
-    print("\nAs a public user, what would you like to do?")
-    print("1. Send an encrypted message \n2. Authenticate a digital signature \n3. Exit")
-    
-
-if choice == 2:
-    print(2)
-
-if choice==3:
-    print(3)
- 
-
-    
+# Runs the main menu
+menu()

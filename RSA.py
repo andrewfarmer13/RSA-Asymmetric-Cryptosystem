e501a069786d9242ebcf73532ceb994b00a69f75
andrewfarmer13 <andrewfarmer13@gmail.com>
2022-02-07 13:09:21 -0600

    Update RSA.py
    Added public key generation and rearranged menu into main.

--- RSA.py
+++ RSA.py
@@ -1,9 +1,18 @@
 ##Contributers: Andrew Farmer, Clayton McEntire
-
+import math
+import random
 
 ##Generates both public and private keys
 def generate():
-    print("RSA keys have been generated.")
+    divisor = 2
+    while(divisor != 1):
+        e = random.randint(0, 10200)
+        divisor = math.gcd(e, 10200)
+        
+    
+    print("Divisor: " + str(divisor))
+    print("Public Key: " + str(e))
+    
     
 ##Generate digital signature
 def digital():
@@ -14,43 +23,29 @@ def authenticate():
     print("test")
     
 ##Encrypts with a public key
-def encrypt():
+def encrypt(str):
     print("test")
     
 ##Decrypts with a private key
 def decrypt():
     print("test")
     
-## Main Selection Menu
-def menu():
-    print("\nPlease select your user type:")
-    print("1. Public User \n2. The owner of the keys \n3. Exit Program")
-    choice = None
-    choice = int(input("\nPlease Enter your choice: "))  
-    if choice == 1:
-        publicusr()
-    elif choice == 2:
-        owner()
-    elif choice ==3:
-        exit()
-        
 ## Public User
 def publicusr():
      print("\nAs a public user, what would you like to do?")
      print("1. Send an encrypted message \n2. Authenticate a digital signature \n3. Exit")
-     choice = None
      choice = int(input("Enter your choice: "))
      if choice ==1:
         string = None
         string = str(input("Enter a message: "))
-                
+        encrypt(string)
         ## CALL ENCRYPT AND send string
         print("Message encrypted and sent.")
      if choice ==2:
         print("TEST REPLACE LATER")
     #Exit to menu
      elif choice == 3:
-        menu()
+         exit()
 
 ## Owner of keys
 def owner():
@@ -60,13 +55,17 @@ def owner():
 ##Driver Function
 def main():
     generate()
-    digital()
-    authenticate()
-    encrypt()
-    decrypt()
-   ## menu()
-   ## publicusr()
+    choice = 0;
+    
+    while(choice != 3):
+        print("\nPlease select your user type:")
+        print("1. Public User \n2. The owner of the keys \n3. Exit Program")
+        choice = int(input("\nPlease Enter your choice: "))  
+        if choice == 1:
+            publicusr()
+        elif choice == 2:
+            owner()
+    
+        
     
 main()
-# Runs the main menu
-menu()

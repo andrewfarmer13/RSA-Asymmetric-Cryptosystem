7e651ca8d1a142ac451f2150c39ce9c12ecde187
andrewfarmer13 <andrewfarmer13@gmail.com>
2022-02-07 14:51:14 -0600

    Update RSA.py

--- RSA.py
+++ RSA.py
@@ -4,16 +4,32 @@ import random
 
 ##Generates both public and private keys
 def generate():
+    
+    ##We need to add a generate prime number function to get a larger n
+    n = 10200
+    
+    ##Generates public Key
     divisor = 2
     while(divisor != 1):
-        e = random.randint(0, 10200)
-        divisor = math.gcd(e, 10200)
+        e = random.randint(0, n)
+        divisor = math.gcd(e, n)
         
+    x, y,d = extended_gcd(e, n)
+    
     
     print("Divisor: " + str(divisor))
     print("Public Key: " + str(e))
+    print("Private Key: " + str(x))
+    return ((e,n),(x,n))
     
-    
+ ##Extended GCD
+def extended_gcd(a =1, b = 1):
+    if b == 0:
+        return (1, 0, a)
+    (x, y, d) = extended_gcd(b, a%b)
+    return y, x - a//b*y, d
+
+   
 ##Generate digital signature
 def digital():
     print("test")
@@ -23,18 +39,43 @@ def authenticate():
     print("test")
     
 ##Encrypts with a public key
-def encrypt(str):
-    print("test")
+def encrypt(string, pubkey):
+   key, n = pubkey
+   
+   ##Not sold on this, just the intial idea
+   ciphertext = ""
+   string = string.upper()
+   for x in string:
+       x = ord(x)
+       a = chr((x**key)%n)
+       ciphertext += a
+
+   return ciphertext
+        
     
 ##Decrypts with a private key
 def decrypt():
     print("test")
     
 ## Public User
-def publicusr():
+def publicusr(pubKey):
      print("\nAs a public user, what would you like to do?")
      print("1. Send an encrypted message \n2. Authenticate a digital signature \n3. Exit")
      choice = int(input("Enter your choice: "))
+<<<<<<< HEAD
+    
+     while(choice != 3):
+         if (choice ==1):
+            string = str(input("Enter a message: "))
+            ciphertext = encrypt(string, pubKey)
+            ## CALL ENCRYPT AND send string
+            print(ciphertext)
+            break
+         elif (choice ==2):
+            print("TEST REPLACE LATER")
+   
+     
+=======
      if choice ==1:
         string = None
         string = str(input("Enter a message: "))
@@ -45,16 +86,17 @@ def publicusr():
         print("TEST REPLACE LATER")
     #Exit to menu
      elif choice == 3:
-         exit()
+         main()
+>>>>>>> 3deeff7f572cbed402bf952c21bb6162bd52ba20
 
 ## Owner of keys
-def owner():
-    print("temp")
+def owner(privkey):
+    print("test")
 
 
 ##Driver Function
 def main():
-    generate()
+    public, private = generate()
     choice = 0;
     
     while(choice != 3):
@@ -62,10 +104,10 @@ def main():
         print("1. Public User \n2. The owner of the keys \n3. Exit Program")
         choice = int(input("\nPlease Enter your choice: "))  
         if choice == 1:
-            publicusr()
+            publicusr(public)
         elif choice == 2:
-            owner()
+            owner(private)
     
         
-    
+## Starts Main   
 main()

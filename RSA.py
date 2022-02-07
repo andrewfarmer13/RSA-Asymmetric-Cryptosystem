2fbb37a90940fabe1c1b702c7a4d4457cfad6dbd
ClaytonMcEntire <83796599+ClaytonMcEntire@users.noreply.github.com>
2022-02-07 09:58:56 -0600

    UI Impementation Began

--- RSA.py
+++ RSA.py
@@ -1,9 +1,9 @@
-##Contributers: Andrew Farmer, Clayton 
+##Contributers: Andrew Farmer, Clayton McEntire
 
 
 ##Generates both public and private keys
 def generate():
-    print("test")
+    print("RSA keys have been generated.")
     
 ##Generate digital signature
 def digital():
@@ -32,4 +32,23 @@ def main():
     decrypt()
     
 main()
+
+ 
+print("Please select your user type:")
+print("1. Public User \n2. The owner of the keys \n3. Exit Program")
+choice = None
+choice = int(input("\nPlease Enter your choice: "))
+    
+if choice == 1:
+    print("\nAs a public user, what would you like to do?")
+    print("1. Send an encrypted message \n2. Authenticate a digital signature \n3. Exit")
+    
+
+if choice == 2:
+    print(2)
+
+if choice==3:
+    print(3)
+ 
+
     
